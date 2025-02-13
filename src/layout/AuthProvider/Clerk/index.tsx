cb604c6063436c42e9a9c0bd6b5962419a74ec70
tardis <14971857@qq.com>
2025-02-13 15:59:37 +0800

    feat: update cdn

--- src/layout/AuthProvider/Clerk/index.tsx
+++ src/layout/AuthProvider/Clerk/index.tsx
@@ -45,7 +45,7 @@ const Clerk = memo(({ children }: PropsWithChildren) => {
     <ClerkProvider
       appearance={updatedAppearance}
       // 自有 https://www.npmjs.com/package/@clerk/clerk-js?activeTab=readme
-      clerkJSUrl="https://npm.webcache.cn/@clerk/clerk-js@5/dist/clerk.browser.js"
+      clerkJSUrl="https://npm.akass.cn/@clerk/clerk-js@5.52.2/dist/clerk.browser.js"
       dynamic
       localization={localization}
       signUpUrl={!enableClerkSignUp ? '/login' : '/signup'} // Redirect sign-up to sign-in if disabled
